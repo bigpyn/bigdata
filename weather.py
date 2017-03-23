f94703beac22fc9941691aa1bd8792bb3416e057
bigpyn <2489172753@qq.com>
2017-03-23 19:23:24 +0800

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,35 @@
+import urllib.request,sys
+
+import re
+provice=input('输入省名(请使用拼音):');
+url="http://qq.ip138.com/weather/"+provice+'/'
+
+wetherhtml=urllib.request.urlopen(url)
+result=wetherhtml.read().decode('GB2312')#.encode('utf-8')
+print (result)
+
+'''s=<td>
+10℃～
+2℃
+</td>
+<td>
+7℃～
+0℃
+</td>
+<td>
+14℃～
+4℃
+</td>
+<td>
+15℃～
+5℃
+</td>
+<td>
+15℃～
+3℃
+</td>'''
+
+pattern='<td>([0-9]{2}.*?[0-9]{2}.*?)</td>'
+temperature=re.findall(pattern,result)#温度
+print(temperature)
+#把温度取出来，比如：5℃～3℃
