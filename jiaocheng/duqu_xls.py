5331d5c740ff3072fb6bb71ad37fd8e2e5f6fa9b
bigpyn <2489172753@qq.com>
2017-04-07 10:03:55 +0800

    Create duqu_xls.py

---
+++ jiaocheng/duqu_xls.py
@@ -0,0 +1,24 @@
+#写Python 脚本，读这个文件：
+
+
+#! /usr/bin/env python
+#coding=utf-8
+
+# pyexcel_xls 以 OrderedDict 结构处理数据
+from collections import OrderedDict
+
+from pyexcel_xls import get_data
+from pyexcel_xls import save_data
+
+
+def read_xls_file():
+    xls_data = get_data(r"D:\read_test.xlsx")
+    print "Get data type:", type(xls_data)
+    for sheet_n in xls_data.keys():
+        print sheet_n, ":", xls_data[sheet_n]
+
+
+if __name__ == '__main__':
+    read_xls_file()
+
+
