8b9a6c8e5a31fc60c5925b0b88f0a2008d0e9b93
bigpyn <2489172753@qq.com>
2017-04-07 10:16:29 +0800

    Update duqu_xls_2.py

--- jiaocheng/duqu_xls_2.py
+++ jiaocheng/duqu_xls_2.py
@@ -1,3 +1,52 @@
+#-*- encoding: utf-8 -*-
+# from html.parser import HTMLParser
+
+# class MyParser(HTMLParser):
+    # def __init__(self):
+        # HTMLParser.__init__(self)        
+        
+    # def handle_starttag(self, tag, attrs):
+        # # 这里重新定义了处理开始标签的函数
+        # if tag == 'a':
+            # # 判断标签<a>的属性
+            # for name,value in attrs:
+                # if name == 'href':
+                    # print (value)
+        
+
+# if __name__ == '__main__':
+    # a = '<html><head><title>test</title><body><a href="http://www.163.com">链接到163</a></body></html>'
+    
+    # my = MyParser()
+    # # 传入要分析的数据，是html的。
+    # my.feed(a)
+
+# def power(x,y=2):
+	# r = 1
+	# for i in range(y):
+		# r = r * x
+	# return r
+# print(power(3))
+# print(power(3,3))
+#! /usr/bin/env python
+#coding=utf-8
+
+# pyexcel_xls 以 OrderedDict 结构处理数据
+# from collections import OrderedDict
+
+# from pyexcel_xls import get_data
+# from pyexcel_xls import save_data
+
+
+# def read_xls_file():
+    # xls_data = get_data(r"D:\read_test.xlsx")
+    # print ("Get data type:", type(xls_data))
+    # for sheet_n in xls_data.keys():
+        # print (sheet_n, ":", xls_data[sheet_n])
+
+
+# if __name__ == '__main__':
+    # read_xls_file()
 #! /usr/bin/env python
 #coding=utf-8
 
